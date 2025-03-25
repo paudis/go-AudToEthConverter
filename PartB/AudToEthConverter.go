ae9d09c9173efe7f195ca15501b839be3ff26fdf
Paudi <103873480+paudis@users.noreply.github.com>
2025-03-25 13:22:09 +1000

    Added application structure and outline

---
+++ PartB/AudToEthConverter.go
@@ -0,0 +1,29 @@
+/*
+Packages:
+- net/http to make HTTP requests to external API fetchers: Get(), NewRequest(), Response
+- encoding/json to parse API responses: Unmarshal(), Marshall()
+- sync for concurrnecy: WaitGroup(), Mutex()
+- time to set timeouts for scheduling
+- errors for explicitl handling errors: eg: timeouts, failures
+- fmt for printing conversion results to the CLI
+- io to handle raw responses from HTTP requests
+
+Functions:
+
+Main Function:
+Where the user will be prompted by the CLI to enter an amount to be converted from AUD to ETH.
+Error handling to sanitize user input
+
+*/
+
+// Package & imports
+
+// func convertAmount() {}
+
+// func fetchRate() {}
+
+// func aggregateRates(){}
+
+// func returnResult() {}
+
+// func main() {}
